180047659c0c18862518e755e23ff206de0c699b
Mehnazahmed <mehnazchowdhury613@gmail.com>
2022-10-15 17:16:10 +0600

    new

---
+++ src/Components/Register.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            {/* <form onSubmit={handleRegister}>
+        <input onBlur={handleEmailBlur} type="email" name='email' id='' placeholder='Your Email' />
+        <br />
+        <input onBlur={handlePasswordBlur} type="password" name='password' id='' placeholder='Your password' />
+        <br />
+        <button type='submit'>Register</button>
+      </form> */}
+            
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDlSIfU_fEdbJnrTeBqhdQRuq9ZTj4wqwM",
+    authDomain: "email-password-auth-13cec.firebaseapp.com",
+    projectId: "email-password-auth-13cec",
+    storageBucket: "email-password-auth-13cec.appspot.com",
+    messagingSenderId: "830790344448",
+    appId: "1:830790344448:web:c84cc2784cdea648df1de5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
